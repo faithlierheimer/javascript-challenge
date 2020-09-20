2df005db64cda5acdb19e8ab72c90ac14c07582f
faithlierheimer <flierheimer@gmail.com>
2020-09-20 10:22:10 -0600

    selected table itself and table body after a lot of false starts lol

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,9 @@
-// from data.js
+// Grab rows from from data.js
 var tableData = data;
+//select table element
+var table = d3.select('table');
+//stripe the table using bootstrap for readability
+table.attr('class', 'table-striped')
 
-// YOUR CODE HERE!
+//select table body in order to append new rows
+var tbody = d3.select('tbody');
