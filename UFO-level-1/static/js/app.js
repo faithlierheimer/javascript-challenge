ec61a02034f5c5ba41255d131f218e4d7c75ae12
faithlierheimer <flierheimer@gmail.com>
2020-09-20 11:31:21 -0600

    figured out how to add to each column!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,28 +1,33 @@
+
 // Grab rows from from data.js
 var tableData = data;
 //select table element
 var table = d3.select('table');
 //stripe the table using bootstrap for readability
+
 table.attr('class', 'table-striped')
 
 //select table body in order to append new rows
 var tbody = d3.select('tbody');
 
-//append one new row for practice--may need to put into for loop?
-var practiceRow = tbody.append('tr');
 
-//append 7 new cells in this row for each data piece? 
-// for(i = 0; i<8; i++){
-//     var dataPractice = practiceRow.append('td');
-//     dataPractice.text(data[0][datetime]);
-// }
-
-for(i = 0; i <8; i++){
+//add cities to their own column
+for(let i = 0; i < tableData.length; i++){
     var row = tbody.append('tr');
-    for(i = 0; i<8; i++){
-        var cell = row.append('td');
-        cell.text(tableData[0].city);
-    }
-    
+    var cell = row.append('td');
+    var cellCity = row.append('td');
+    var cellState = row.append('td');
+    var cellCountry = row.append('td');
+    var cellShape = row.append('td');
+    var cellDuration = row.append('td');
+    var cellComments = row.append('td');
+    cell.text(tableData[i].datetime); 
+    cellCity.text(tableData[i].city);
+    cellState.text(tableData[i].state);
+    cellCountry.text(tableData[i].country);
+    cellShape.text(tableData[i].shape);
+    cellDuration.text(tableData[i].durationMinutes);
+    cellComments.text(tableData[i].comments);
 }
-console.log(tableData[0].city)
+
+
