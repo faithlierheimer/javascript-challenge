c35f75d762914b453bbf6b477e4a6d72d2fe5284
faithlierheimer <flierheimer@gmail.com>
2020-09-20 10:39:07 -0600

    successfully added some test data across a row instead of columns! let's go

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,3 +7,22 @@ table.attr('class', 'table-striped')
 
 //select table body in order to append new rows
 var tbody = d3.select('tbody');
+
+//append one new row for practice--may need to put into for loop?
+var practiceRow = tbody.append('tr');
+
+//append 7 new cells in this row for each data piece? 
+// for(i = 0; i<8; i++){
+//     var dataPractice = practiceRow.append('td');
+//     dataPractice.text(data[0][datetime]);
+// }
+
+for(i = 0; i <8; i++){
+    var row = tbody.append('tr');
+    for(i = 0; i<8; i++){
+        var cell = row.append('td');
+        cell.text(tableData[0].city);
+    }
+    
+}
+console.log(tableData[0].city)
