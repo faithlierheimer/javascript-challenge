77da16f870ec765bac67ad77203b4f2c8c79fd4c
faithlierheimer <flierheimer@gmail.com>
2020-09-20 19:32:28 -0600

    table will filter correctly based on input value, but now the issue is figuring out how to get the filtered data to re render on the page.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -31,26 +31,46 @@ for(let i = 0; i < tableData.length; i++){
 }
 
 //select form itself--defined in the html
-var form = d3.select("#form");
+var form = d3.select('form');
 //Select button that submits form-defined in the HTML
-var button = d3.select("#filter-btn");
-
+console.log(form)
 //Create event handlers so Javascript knows what to do when button is 
 //clicked/form is submitted 
-button.on("click", runEnter);
 form.on("submit", runEnter);
 
 //define form submission function--e.g. what to do when form is submitted
 function runEnter(){
-    //prevent page from refreshing
+     //prevent page from refreshing
     d3.event.preventDefault();
     //select input element, get raw html node
-    var inputElement = d3.select("#form-control");
+    var inputElement = d3.select('input');
     //grab value of input
-    var inputValue = inputElement.property("value");
-    //attempt to filter display table by input date
-    var filtered = table.filter(entry => entry.datetime === inputValue);
-    //check work, consolelog input value
+    var inputValue = inputElement.property('value');
     console.log(inputValue)
+    // //attempt to filter display table by input date
+    var filtered = tableData.filter(tableData => tableData.datetime === inputValue);
+    //check work, consolelog input value
+    console.log(inputElement)
     console.log(filtered)
+    var table = d3.select('table');
+//select table body in order to append new rows
+    var tbody = d3.select('tbody');
+    for(let i = 0; i < filtered.length; i++){
+        var row = tbody.append('tr');
+        var cell = row.append('td');
+        var cellCity = row.append('td');
+        var cellState = row.append('td');
+        var cellCountry = row.append('td');
+        var cellShape = row.append('td');
+        var cellDuration = row.append('td');
+        var cellComments = row.append('td');
+        cell.text(filtered[i].datetime); 
+        cellCity.text(filtered[i].city);
+        cellState.text(filtered[i].state);
+        cellCountry.text(filtered[i].country);
+        cellShape.text(filtered[i].shape);
+        cellDuration.text(filtered[i].durationMinutes);
+        cellComments.text(filtered[i].comments);
+    }
 }
+
